34545b71adde8429d4ba33c240012385ced2b490
luke2327 <qwepoi8526@gmail.com>
2020-03-06 09:26:38 +0900

    index 修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
-const Home = ({ userAgent }) => <h1>user agent: {userAgent}</h1>;
+/** import component */
+import { NextPage } from 'next';
+import { Fragment } from 'react';
+import { DatePicker } from 'antd';
+
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => {
+  return (
+    <Fragment>
+      <h1>user agent: {userAgent}</h1>
+      <DatePicker></DatePicker>
+    </Fragment>
+  );
+}
 
 Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
 
   return { userAgent };
 };
